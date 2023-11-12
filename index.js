09ce3015b1164ec43f8a63bfff369ffcc05756a5
Ismail Hossain <mdismail645221@gmail.com>
2023-11-12 23:07:48 +0600

    added the review get api route

--- index.js
+++ index.js
@@ -1,70 +1,67 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors')
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 
-// Middle ware 
-app.use(cors())
-app.use(express.json())
-
+// Middle ware
+app.use(cors());
+app.use(express.json());
 
 // pass
 // OzOBj7vXvKuIaxoo
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yzc4fwf.mongodb.net/?retryWrites=true&w=majority`;
-console.log({uri})
-
+console.log({ uri });
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  
-  
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-      const menuCollections = client.db('bisstroDB').collection('menu');
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-        
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-      app.get('/menu', async(req, res)=> {
-        const data = await menuCollections.find({}).toArray();
-        res.send(data)
-      })
+    const menuCollections = client.db("bisstroDB").collection("menu");
+    const reviewsCollections = client.db("bisstroDB").collection("review");
 
-            
+    // reviews get API route
+    app.get("/review", async (req, res) => {
+      const data = await reviewsCollections.find({}).toArray();
+      res.send(data);
+    });
 
-
-
-
-
-
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
+    // menu get API route
+    app.get("/menu", async (req, res) => {
+      const data = await menuCollections.find({}).toArray();
+      res.send(data);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send(
+    `<h1 style="color: red; font-size: 2rem; display: flex; align-items: center; justify-content: center; height: 100vh">bisstro boss server is running</h1>`
+  );
+});
 
-app.get('/', (req, res)=> {
-    res.send(`<h1 style="color: red; font-size: 2rem; display: flex; align-items: center; justify-content: center; height: 100vh">bisstro boss server is running</h1>`)
-})
-
-
-app.listen(port, ()=> {
-    console.log('bisstro boss server is running')
-})
+app.listen(port, () => {
+  console.log("bisstro boss server is running");
+});
